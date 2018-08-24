e13764f380350c5325a93ec9de7eb0936eb68787
agudelotmateo <agudelotmateo@gmail.com>
2018-08-24 03:06:37 -0500

    Add code to create gameboard from user input

--- src/main/java/com/agudelotmateo/minesweeper/App.java
+++ src/main/java/com/agudelotmateo/minesweeper/App.java
@@ -1,11 +1,47 @@
 package com.agudelotmateo.minesweeper;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 /**
- * pending!
+ * Minesweeper's entry point.
+ * 
+ * @author Mateo Agudelo Toro
  */
 public class App {
     public static void main(String[] args) {
         // pending!
-        System.out.println(new Cell());
+        Board gameboard = createBoardFromStandardInput();
+    }
+
+    /**
+     * Creates the gameboard based on the specifications given by the user through
+     * the standard input.
+     * 
+     * @return the gameboard according to the input specifications
+     */
+    private static Board createBoardFromStandardInput() {
+        // Print welcome and initial instructions
+        System.out.println("Welcome to Minesweeper!");
+        System.out.println("Please input three space-separated numbers indicating the board's"
+                + " height, width and number of mines respectively: ");
+
+        // Read and validate input
+        Scanner sc = new Scanner(System.in);
+        Board gameboard = null;
+        while (gameboard == null) {
+            int rows = sc.nextInt();
+            int columns = sc.nextInt();
+            int bombs = sc.nextInt();
+            try {
+                gameboard = new Board(rows, columns, bombs);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage() + ". Please try again:");
+            }
+        }
+
+        // Return when valid
+        sc.close();
+        return gameboard;
     }
 }
