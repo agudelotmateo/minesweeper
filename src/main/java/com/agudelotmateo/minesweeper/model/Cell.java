ce569fcd4aa6ef2552a0dda4820d4fc1ca378095
agudelotmateo <agudelotmateo@gmail.com>
2018-08-25 00:46:28 -0500

    Add javadoc to cell model

--- src/main/java/com/agudelotmateo/minesweeper/model/Cell.java
+++ src/main/java/com/agudelotmateo/minesweeper/model/Cell.java
@@ -1,7 +1,9 @@
 package com.agudelotmateo.minesweeper.model;
 
 /**
- * pending!
+ * Represents a cell of the gameboard.
+ * 
+ * @author Mateo Agudelo Toro
  */
 class Cell {
     private boolean hasBomb;
@@ -10,7 +12,9 @@ class Cell {
     private int adjacentCounter;
 
     /**
-     * pending!
+     * Creates a new cell.
+     * 
+     * @param hasBomb whether this cell contains a bomb
      */
     public Cell(boolean hasBomb) {
         this.hasBomb = hasBomb;
@@ -20,49 +24,58 @@ class Cell {
     }
 
     /**
-     * pending!
+     * Checks if the cell has a bomb.
+     * 
+     * @return whether this cell contains a bomb
      */
     public boolean hasBomb() {
         return hasBomb;
     }
 
     /**
-     * pending!
+     * Checks if the cell has been flagged by the user.
+     * 
+     * @return whether this cell is flagged
      */
     public boolean isFlagged() {
         return isFlagged;
     }
 
     /**
-     * pending!
+     * Checks if the cell is covered.
+     * 
+     * @return whether this cell is covered
      */
     public boolean isCovered() {
         return isCovered;
     }
 
     /**
-     * pending!
+     * Gets the number of bombs adjacent to the cell.
+     * 
+     * @return the number of bombs adjacent to the cell
      */
     public int adjacentCount() {
         return this.adjacentCounter;
     }
 
     /**
-     * pending!
+     * Toggles the flagged state of the cell. That is, if the cell is not flagged
+     * before calling this method, it will be flagged after it runs, and viceversa.
      */
     public void toggleFlag() {
         this.isFlagged = !this.isFlagged;
     }
 
     /**
-     * pending!
+     * Sets the state of the cell to uncovered.
      */
     public void uncover() {
         this.isCovered = false;
     }
 
     /**
-     * pending!
+     * Sets the number of bombs adjacent to the cell.
      */
     public void setAdjacentCounter(int adjacentCounter) {
         this.adjacentCounter = adjacentCounter;
